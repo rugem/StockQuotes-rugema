18ec3232568222ff5381d91c12a76f03bee9f231
rugem <rugemam@g.cofc.edu>
2017-03-14 12:21:19 -0400

    Initial Commit

---
+++ app/src/main/java/edu/cofc/csci490/stockquotes_rugema/StockQuotesMainActivity.java
@@ -0,0 +1,13 @@
+package edu.cofc.csci490.stockquotes_rugema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StockQuotesMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_quotes_main);
+    }
+}
